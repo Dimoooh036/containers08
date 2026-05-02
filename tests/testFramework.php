cd9387f58d8479c2251dee30bcd158aa189d6eae
Dimoooh036 <Dmitryhi2006@gmail.com>
2026-05-02 21:39:45 +0300

    ci test

--- tests/testFramework.php
+++ tests/testFramework.php
@@ -1,85 +1,46 @@
 <?php
 
-require_once __DIR__ . '/testframework.php';
-require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/../modules/database.php';
-require_once __DIR__ . '/../modules/page.php';
-
-$testFramework = new TestFramework();
-
-function testDbConnection() {
-    global $config;
-    $db = new Database($config["db"]["path"]);
-    return assertExpression($db != null, "DB connected", "DB failed");
+function message($type, $message) {
+    $time = date('Y-m-d H:i:s');
+    echo "{$time} [{$type}] {$message}" . PHP_EOL;
 }
 
-function testDbCount() {
-    global $config;
-    $db = new Database($config["db"]["path"]);
-    return assertExpression($db->Count("page") >= 3);
+function info($message) {
+    message('INFO', $message);
 }
 
-function testDbCreate() {
-    global $config;
-    $db = new Database($config["db"]["path"]);
-
-    $id = $db->Create("page", [
-        "title" => "Test",
-        "content" => "Test content"
-    ]);
-
-    return assertExpression($id > 0);
+function error($message) {
+    message('ERROR', $message);
 }
 
-function testDbRead() {
-    global $config;
-    $db = new Database($config["db"]["path"]);
-
-    $data = $db->Read("page", 1);
-    return assertExpression($data['title'] === 'Page 1');
+function assertExpression($expression, $pass = 'Pass', $fail = 'Fail'): bool {
+    if ($expression) {
+        info($pass);
+        return true;
+    }
+    error($fail);
+    return false;
 }
 
-function testDbUpdate() {
-    global $config;
-    $db = new Database($config["db"]["path"]);
+class TestFramework {
+    private $tests = [];
+    private $success = 0;
 
-    $db->Update("page", 1, ["title" => "Updated"]);
-    $data = $db->Read("page", 1);
+    public function add($name, $test) {
+        $this->tests[$name] = $test;
+    }
 
-    return assertExpression($data['title'] === 'Updated');
+    public function run() {
+        foreach ($this->tests as $name => $test) {
+            info("Running test {$name}");
+            if ($test()) {
+                $this->success++;
+            }
+            info("End test {$name}");
+        }
+    }
+
+    public function getResult() {
+        return "{$this->success} / " . count($this->tests);
+    }
 }
-
-function testDbDelete() {
-    global $config;
-    $db = new Database($config["db"]["path"]);
-
-    $id = $db->Create("page", ["title"=>"Del","content"=>"Del"]);
-    $db->Delete("page", $id);
-
-    $data = $db->Read("page", $id);
-    return assertExpression($data === null);
-}
-
-function testPageRender() {
-    $page = new Page(__DIR__ . '/../templates/index.tpl');
-
-    $html = $page->Render([
-        "title" => "Hello",
-        "content" => "World"
-    ]);
-
-    return assertExpression(strpos($html, "Hello") !== false);
-}
-
-// добавление тестов
-$testFramework->add('DB connection', 'testDbConnection');
-$testFramework->add('DB count', 'testDbCount');
-$testFramework->add('DB create', 'testDbCreate');
-$testFramework->add('DB read', 'testDbRead');
-$testFramework->add('DB update', 'testDbUpdate');
-$testFramework->add('DB delete', 'testDbDelete');
-$testFramework->add('Page render', 'testPageRender');
-
-// запуск
-$testFramework->run();
-echo $testFramework->getResult();
